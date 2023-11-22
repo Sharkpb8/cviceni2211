6e801c56a4d4fa547fc7f82ea6ab8995b4c8aa48
sharkpb8 <sharkpb8@gmail.com>
2023-11-22 08:36:47 +0100

    easy guesing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const randomNumber = Math.floor(Math.random() * 1001);
+const easyrandomNumber = Math.floor(Math.random() * 99+1);
 function guesing(){
     let x = document.getElementById("numb").value;
     let answer;
@@ -10,5 +11,18 @@ function guesing(){
     document.getElementById("demo").innerHTML = answer;
 }
 
-let r = randomNumber
+function easyguesing(){
+    let x = document.getElementById("easynumb").value;
+    let answer;
+    if(x == easyrandomNumber){
+        answer = "correct";
+    }else{
+        answer = "wrong";
+    }
+    document.getElementById("easydemo").innerHTML = answer;
+}
+
+let r = randomNumber;
+let a = easyrandomNumber;
 console.log(r);
+console.log(a);
